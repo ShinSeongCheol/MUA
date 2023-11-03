37a6d642c499bed8562a21df3f7ae801a178590e
Shin_Seong_Cheol <cher3725@naver.com>
2023-11-03 23:01:31 +0900

    World 모델 추가

--- mua/models.py
+++ mua/models.py
@@ -5,3 +5,7 @@ class User(db.Model):
     id = db.Column(db.String(32), primary_key=True)
     password = db.Column(db.String(256), nullable=False)
     email = db.Column(db.String(64), unique=True, nullable=False)
+
+class World(db.Model):
+    name=db.Column(db.String(32), primary_key=True)
+    type=db.Column(db.String(32), nullable=False)
