d5de5e3e7e0564f242f6347d664465bd60e2673c
Shin_Seong_Cheol <cher3725@naver.com>
2023-11-04 04:01:36 +0900

    컬러명 수정 id -> user_name

--- mua/models.py
+++ mua/models.py
@@ -2,7 +2,7 @@ from mua import db
 
 
 class User(db.Model):
-    id = db.Column(db.String(32), primary_key=True)
+    user_name = db.Column(db.String(32), primary_key=True)
     password = db.Column(db.String(256), nullable=False)
     email = db.Column(db.String(64), unique=True, nullable=False)
 
