9f5b2d9431ed762434d4b5d2dc0f133036ba24b7
Shin_Seong_Cheol <cher3725@naver.com>
2023-11-08 21:11:49 +0900

    스크랩 테스트 추가

--- test/scrap.py
+++ test/scrap.py
@@ -13,6 +13,74 @@ class Unittest(unittest.TestCase):
         response = scrap.getMapleHomeResponse()
         self.assertEqual(response.status_code, 200)
 
+    def test_getWholeWorldRank(self):
+        """
+        전체월드 랭킹 스크랩
+        """
+        URL = "https://maplestory.nexon.com/N23Ranking/World/Total?w=0"
+        scrap.getRank(URL)
+        self.assertEqual()
+
+    def test_getAuroraWorldRank(self):
+        """
+        오로라 랭킹 스크랩
+        """
+
+    def test_getRedWorldRank(self):
+        """
+        레드 랭킹 스크랩
+        """
+
+    def test_getEnosisWorldRank(self):
+        """
+        이노시스 랭킹 스크랩
+        """
+
+    def test_getUnionWorldRank(self):
+        """
+        유니온 랭킹 스크랩
+        """
+
+    def test_getScaniaWorldRank(self):
+        """
+        스카니아 랭킹 스크랩
+        """
+
+    def test_getLunaWorldRank(self):
+        """
+        루나 랭킹 스크랩
+        """
+
+    def test_getZenithWorldRank(self):
+        """
+        제니스 랭킹 스크랩
+        """
+
+    def test_getCroixWorldRank(self):
+        """
+        크로아 랭킹 스크랩
+        """
+
+    def test_getVeraWorldRank(self):
+        """
+        베라 랭킹 스크랩
+        """
+
+    def test_getElysiumWorldRank(self):
+        """
+        엘리시움 랭킹 스크랩
+        """
+
+    def test_getArcaneWorldRank(self):
+        """
+        아케인 랭킹 스크랩
+        """
+
+    def test_getNovaWorldRank(self):
+        """
+        노바 랭킹 스크랩
+        """
+
     def tearDown(self):
         pass
 
