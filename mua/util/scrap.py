4fbd56d0dc1133846208c83d28f6d079f11f500d
Shin_Seong_Cheol <cher3725@naver.com>
2023-11-08 20:49:12 +0900

    메이플 응답 코드 수정

--- mua/util/scrap.py
+++ mua/util/scrap.py
@@ -3,6 +3,12 @@ from bs4 import BeautifulSoup
 
 
 def getMapleHomeResponse():
+    """
+    메이플 홈페이지 응답
+    """
     MAPLE_HOME_URL = "https://maplestory.nexon.com/Home/Main"
     response = requests.get(MAPLE_HOME_URL)
-    return response.status_code
+    return response
+
+
+
