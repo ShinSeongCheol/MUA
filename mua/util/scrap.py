165634891d36b6530b1b3b67152d64496f9a3608
Shin_Seong_Cheol <cher3725@naver.com>
2023-11-08 22:41:43 +0900

    캐릭터 정보 스크랩 추가

--- mua/util/scrap.py
+++ mua/util/scrap.py
@@ -11,4 +11,66 @@ def getMapleHomeResponse():
     return response
 
 
+def getCharacterInfo(URL: str):
+    # URL의 페이지 정보
+    response = requests.get(URL)
 
+    # 정상 응답이 아니면 종료
+    if not response.ok:
+        return
+
+    # 뷰티풀 수프 객체 생성
+    bs = BeautifulSoup(response.text, "html.parser")
+    table_row = bs.select(
+        "#container > div > div > div:nth-child(4) > div.rank_table_wrap > table > tbody > tr"
+    )
+
+    # 캐릭터 정보 리스트
+    character_info_list = []
+
+    # 10위까지 반복
+    for i in range(10):
+        # 랭크
+        rank = table_row[i].select("td > p")[0]
+        if rank.find("img"):
+            rank = rank.select("img")[0]["alt"][0:1]
+        else:
+            rank = rank.text.split()[0]
+
+        # 캐릭터 이미지
+        image = table_row[i].select("td > span > img")[0]['src']
+
+        # 캐릭터 이름
+        name = table_row[i].select("td > dl > dt > a")[0].text
+
+        # 캐릭터 직업
+        occupation = table_row[i].select("td > dl > dd")[0].text.split("/")[1].strip()
+
+        # 캐릭터 레벨
+        level = table_row[i].select("td:nth-child(3)")[0].text.split("Lv.")[1]
+
+        # 캐릭터 경험치
+        expreience = table_row[i].select("td:nth-child(4)")[0].text
+
+        # 캐릭터 인기도
+        popularity = table_row[i].select("td:nth-child(5)")[0].text
+
+        # 캐릭터 길드
+        guild = table_row[i].select("td:nth-child(6)")[0].text
+
+        # 캐릭터 정보 딕셔너리 생성
+        character_info = {
+            "rank":rank,
+            "image":image,
+            "name":name,
+            "occupation":occupation,
+            "level":level,
+            "expreience":expreience,
+            "popularity":popularity,
+            "guild":guild
+        }
+
+        # 캐릭터 정보 리스트에 딕셔너리 추가
+        character_info_list.append(character_info)
+
+    return character_info_list
--- test/scrap.py
+++ test/scrap.py
@@ -13,70 +13,90 @@ class Unittest(unittest.TestCase):
         response = scrap.getMapleHomeResponse()
         self.assertEqual(response.status_code, 200)
 
-    def test_getWholeWorldRank(self):
+    def test_getWholeWorldCharacterInfo(self):
         """
         전체월드 랭킹 스크랩
         """
         URL = "https://maplestory.nexon.com/N23Ranking/World/Total?w=0"
-        scrap.getRank(URL)
-        self.assertEqual()
+        character_info = scrap.getCharacterInfo(URL)
+        data: dict = character_info[0]
+        rank = data.get("rank")
+        image = data.get("image")
+        name = data.get("name")
+        occupation = data.get("occupation")
+        level = data.get("level")
+        expreience = data.get("expreience")
+        popularity = data.get("popularity")
+        guild = data.get("guild")
 
-    def test_getAuroraWorldRank(self):
+        self.assertEqual(rank, "1")
+        self.assertEqual(
+            image,
+            "https://avatar.maplestory.nexon.com/Character/180/INIMBCNLKFOPHKPADGLEFIAOJAJOKDGMIFCDOMHDJDCCGGMCBFKCCOGBMFIOGHJOPKCDALEHEOMJANJPAJPGNEMKNPGIJGBGNPAFHOJKAAMBFFIEMGLGCIECIGCKBEEHLLBPMBPCJFBABDLIFMFBLCELFEGCIILPOKNFHENJAMGPBFONCPENGFPHCNPMDLEJNLICJLPACPDLHHHAICFPBHECGIOPHDHNEHNGHILJNEGFDMNIIABELDLEMFKFIONL.png",
+        )
+        self.assertEqual(name, "오지환")
+        self.assertEqual(occupation, "키네시스")
+        self.assertEqual(level, "295")
+        self.assertEqual(expreience, "611,251,115,519,887")
+        self.assertEqual(popularity, "28,624")
+        self.assertEqual(guild, "지존")
+
+    def test_getAuroraWorldCharacterInfo(self):
         """
         오로라 랭킹 스크랩
         """
 
-    def test_getRedWorldRank(self):
+    def test_getRedWorldCharacterInfo(self):
         """
         레드 랭킹 스크랩
         """
 
-    def test_getEnosisWorldRank(self):
+    def test_getEnosisWorldCharacterInfo(self):
         """
         이노시스 랭킹 스크랩
         """
 
-    def test_getUnionWorldRank(self):
+    def test_getUnionWorldCharacterInfo(self):
         """
         유니온 랭킹 스크랩
         """
 
-    def test_getScaniaWorldRank(self):
+    def test_getScaniaWorldCharacterInfo(self):
         """
         스카니아 랭킹 스크랩
         """
 
-    def test_getLunaWorldRank(self):
+    def test_getLunaWorldCharacterInfo(self):
         """
         루나 랭킹 스크랩
         """
 
-    def test_getZenithWorldRank(self):
+    def test_getZenithWorldCharacterInfo(self):
         """
         제니스 랭킹 스크랩
         """
 
-    def test_getCroixWorldRank(self):
+    def test_getCroixWorldCharacterInfo(self):
         """
         크로아 랭킹 스크랩
         """
 
-    def test_getVeraWorldRank(self):
+    def test_getVeraWorldCharacterInfo(self):
         """
         베라 랭킹 스크랩
         """
 
-    def test_getElysiumWorldRank(self):
+    def test_getElysiumWorldCharacterInfo(self):
         """
         엘리시움 랭킹 스크랩
         """
 
-    def test_getArcaneWorldRank(self):
+    def test_getArcaneWorldCharacterInfo(self):
         """
         아케인 랭킹 스크랩
         """
 
-    def test_getNovaWorldRank(self):
+    def test_getNovaWorldCharacterInfo(self):
         """
         노바 랭킹 스크랩
         """
