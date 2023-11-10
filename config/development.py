10a4fa68114c392c0e3ff09930d82648e45515ea
Shinseongcheol <cher0531@naver.com>
2023-11-10 16:48:11 +0900

    로깅 환경 수정

--- config/development.py
+++ config/development.py
@@ -1,5 +1,34 @@
 from config.default import *
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'mua.db'))
+SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(os.path.join(BASE_DIR, "mua.db"))
 SQLALCHEMY_TRACK_MODIFICATION = False
 SECRET_KEY = "dev"
+
+from logging.config import dictConfig
+
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+            }
+        },
+        "handlers": {
+            "file": {
+                "level": "INFO",
+                "class": "logging.handlers.RotatingFileHandler",
+                "filename": os.path.join(BASE_DIR, "logs/mua.log"),
+                "maxBytes": 1024 * 1024 * 5,
+                "backupCount": 5,
+                "formatter": "default",
+            },
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "default",
+                "level": "DEBUG",
+            },
+        },
+        "root": {"level": "DEBUG", "handlers": ["file", "console"]},
+    }
+)
