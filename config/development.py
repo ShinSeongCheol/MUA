dd797a5af0e8f0058c8cd75c55d413938610ce64
Shin_Seong_Cheol <cher3725@naver.com>
2023-11-12 16:51:56 +0900

    개발 환경 설정

--- config/development.py
+++ config/development.py
@@ -22,6 +22,7 @@ dictConfig(
                 "maxBytes": 1024 * 1024 * 5,
                 "backupCount": 5,
                 "formatter": "default",
+                "encoding": "utf-8",
             },
             "console": {
                 "class": "logging.StreamHandler",
