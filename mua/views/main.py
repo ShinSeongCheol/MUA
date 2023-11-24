4e4baa003ae25f1f510e5f5d427149a965f06a53
Shin_Seong_Cheol <cher3725@naver.com>
2023-11-25 05:00:00 +0900

    월드별 랭킹 5위 표시 추가

--- mua/views/main.py
+++ mua/views/main.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, url_for, render_template
 from werkzeug.utils import redirect
+from sqlalchemy import cast, INTEGER
 
 from mua.models import *
 
@@ -8,4 +9,21 @@ bp = Blueprint("main", __name__, url_prefix="/")
 
 @bp.route("/")
 def main():
-    return render_template("main/main.html")
+    # 가져올 월드들의 리스트
+    top_characters = {}
+    worlds = World.query.order_by(cast(World.value, INTEGER)).all()
+
+    # 각 월드별로 상위 5개 캐릭터 가져오기
+    for world in worlds:
+        top_characters[world.name] = (
+            db.session.query(Character, Image)
+            .filter_by(world_name=world.name)
+            .order_by(Character.level.desc(), Character.experience.desc())
+            .join(Image, Character.image_id == Image.id)
+            .group_by(Character.nickname)
+            .order_by(Image.update_date.desc())
+            .limit(5)
+            .all()
+        )
+
+    return render_template("main/main.html", top_characters=top_characters)
